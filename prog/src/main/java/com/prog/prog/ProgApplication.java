dee7af229d60ba81b2ea22fb5c74840a6f3ba216
vohizy <99481011+Vohizy@users.noreply.github.com>
2024-09-23 14:47:09 +0300

    feat: create model for patrimoine

---
+++ prog/src/main/java/com/prog/prog/ProgApplication.java
@@ -0,0 +1,13 @@
+package com.prog.prog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgApplication.class, args);
+	}
+
+}
---
+++ prog/src/main/java/com/prog/prog/model/patrimoine.java
@@ -0,0 +1,20 @@
+package com.prog.prog;.model;
+import lombok.ToString;
+import java.time.LocalDateTime;
+public class Employee implements Serializable {
+    private String possesseur,
+    private LocalDatTime derniereModification;
+
+    public String getPossesseur(){
+        return possesseur
+    }
+    public void setPossesseur(){
+        this.possesseur = possesseur;
+    }
+    public LocalDateTime getDerniereModification(){
+        return derniereModification;
+    }
+    public void setDerniereModification(){
+        this.derniereModification = derniereModification
+    }
+}
