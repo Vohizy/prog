078284cedacb8f591d0cec6677043261b807aa89
vohizy <99481011+Vohizy@users.noreply.github.com>
2024-09-23 15:46:10 +0300

    feat: add get mapping

--- prog/src/main/java/com/prog/prog/controller/patrimoineController.java
+++ prog/src/main/java/com/prog/prog/controller/patrimoineController.java
@@ -3,11 +3,13 @@ import com.prog.prog.service.PatrimoineService;
 import com.prog.prog.service.PatrimoineService.*;
 import com.prog.prog.model.patrimoine;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
 import java.io.IOException;
 import java.nio.file.*;
 import java.time.LocalDateTime;
 import java.util.HasMap;
 import java.util.Map;
+
 
 @RestController
 @RequetMapping('/patrimoine')
@@ -23,6 +25,19 @@ public class PatrimoineController{
 
         service.savaPatrimoines(patrimoineDB);
         return patrimoine
+    }
+
+    @GetMapping('/{id}')
+    public ResponseEntity<Patrimoine> getPatrimoine(@PathVariable String id) throws IOException{
+        Map<String, Patrimoine> patrimoineDB = service.loadPatrimoines();
+
+        Patrimoine patrimoine = patrimoineDB.get(id);
+        if(patrimoine!=null){
+            return ResponseEntity.ok(patrimoine);
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+
     }
 
 }
